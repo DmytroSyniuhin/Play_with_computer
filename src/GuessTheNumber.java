12b1e54abad62b6a9493503e8d22b594c513bd19
DmytroSyniuhin <dmytrosyniuhin@gmail.com>
2023-10-18 13:44:09 +0300

    Task using a loop

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -2,29 +2,27 @@ import java.util.Scanner;
 
 public class GuessTheNumber {
     public static void main(String[] args) {
-        int randomNumber = (int) (Math.random() * 11);
-        System.out.println("Вам потрібно вгадати число від 0 до 10 включно, яке загадав комп'ютер. У Вас на це 3 спроби");
-        System.out.println("Спроба №1:");
-
         Scanner scanner = new Scanner(System.in);
-        int scannerName1 = scanner.nextInt();
 
-        if (scannerName1 == randomNumber) {
-            System.out.println("Ви вгадали з першого разу! Неймовірно!");
-        } else {
-            System.out.println("На жаль Ви не вгадали. Спробуйте ще раз:");
-            int scannerName2 = scanner.nextInt();
-            if (scannerName2 == randomNumber) {
-                System.out.println("Вам це вдалося! Вітаю!");
-            } else {
-                System.out.println("Шкода, але знову не вірно( Проте, Ви маєте ще одну спробу:");
-                int scannerName3 = scanner.nextInt();
-                if (scannerName3 == randomNumber) {
-                    System.out.println("З третьої спроби, але Ви зробили це! Вітаю!");
-                } else {
-                    System.out.println("Знову промахнулися. Число, яке загадав комп'ютер було " + randomNumber);
-                }
+        int randomNumber = (int) (Math.random() * 11);
+        int count = 1;
+
+        System.out.println("Вам потрібно відгадати загадане комп'ютером число від 0 до 10 включно. Ви маєте на це 3 спроби.");
+
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Спроба № " + count);
+            count++;
+
+            int scanInt = scanner.nextInt();
+
+            if (randomNumber == scanInt) {
+                System.out.println("Вітаю, Вам вдалося вгадати загадене число!");
+
+                break;
             }
+            System.out.println("На жаль, Ви не вгадали.");
         }
+        System.out.println("Комп'ютер загадав число " + randomNumber + ".");
     }
 }
